86ad4adde8f368259cd1ac984ed6ae05bdeb1388
daniiar1997 <daniiar.abdiev@gmail.com>
2016-02-19 10:41:17 +0600

    Update homeowork5.py

--- homeowork5.py
+++ homeowork5.py
@@ -1,7 +1,8 @@
-x = int(input("Please write number from 1 to 9:"))
+
 class A:
+    x = int(input("Please write number from 1 to 9:"))
     def __init__(self):
-        self.x = x
+        self.x = self.x
     def func(self):
         s = str(input("Please write something:"))
         n = int(input("Please write how many times you want your 'something' to be repeated:"))
@@ -10,17 +11,17 @@ class A:
             return print(_str_rep)
     def func1(self):
         m = int(input("Please write the degree you want your number to be put in:"))
-        return print(x**m)
+        return print(self.x**m)
     def func2(self,x ):
         b = [x for x in range(x, x+10)]
         return print(b)
 a = A()
-if x in range(1, 4):
-    print(a.func(x))
-elif x in range(4, 7):
+if a.x in range(1, 4):
+    print(a.func())
+elif a.x in range(4, 7):
     print(a.func1())
-elif x in range(7, 10):
-    print(a.func2(x))
+elif a.x in range(7, 10):
+    print(a.func2())
 else:
     print("Error")
 
@@ -30,6 +31,18 @@ _age = int(input("Can you tell me your age,please?"))
 class B():
     def __init__(self):
         self._age = _age
+        if _age in range(0, 8):
+            print(self.f())
+        elif _age in range(7, 18):
+            print(self.fu())
+        elif _age in range(18, 26):
+            print(self.fun())
+        elif _age in range(25, 61):
+            print(self.func())
+        elif _age in range(60, 121):
+            print(self.funct())
+        else:
+            print(self.functi())
 
     def f(self):
         print("Kinder-garden, I am sorry")
@@ -53,15 +66,4 @@ class B():
         print("I am sorry, because of our policy aliens can not use this program. \
         Though, you can always try ageforaliens.py")
 b = B()
-if _age in range(0, 8):
-    print(b.f())
-elif _age in range(7, 18):
-    print(b.fu())
-elif _age in range(18, 26):
-    print(b.fun())
-elif _age in range(25, 61):
-    print(b.func())
-elif _age in range(60, 121):
-    print(b.funct())
-else:
-    print(b.functi())
+print(b._age)
