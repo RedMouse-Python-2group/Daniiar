d4832a5d3230c3d85b1dd28982b397171a56e5e2
daniiar1997 <daniiar.abdiev@gmail.com>
2016-04-06 10:29:15 +0600

    Added files via upload

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import url, patterns
-from django.contrib import admin
+from django.conf.urls import url
+from django.views.generic import TemplateView
 from .views import (
     post_list,
     post_useful,
     post_events,
     post_other,
     post_create,
-    post_about_us,
     post_detail,
     post_update,
     post_delete,
@@ -19,7 +18,7 @@ urlpatterns = [
     url(r'^events/$', post_events, name="events"),
     url(r'^other/$', post_other, name="other"),
     url(r'^create/$', post_create, name="create"),
-    url(r'^about/$', post_about_us, name="about"),
+    url(r'^about/$', TemplateView.as_view(template_name='blog/about.html'), name='about'),
     url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name="update"),
     url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name="delete"),
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -13,7 +13,7 @@ def post_create(request):
     # if not request.user.is_staff or not request.user.is_superuser:
     #     raise Http404
     if not request.user.is_authenticated():
-        return redirect("/")
+        return redirect("log_in")
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -29,182 +29,11 @@ def post_create(request):
         return render(request, "blog/post_form.html", context)
 
 
-def post_detail(request, slug=None):
-    if not request.user.is_authenticated():
-        return redirect("/")
-    instance = get_object_or_404(Post, slug=slug)
-    if instance.publish > timezone.now().date() or instance.draft:
-        if not request.user.is_staff or not request.user.is_superuser:
-            raise Http404
-    share_string = quote_plus(instance.content)
-    context = {
-        "title": instance.title,
-        "instance": instance,
-        "share_string": share_string,
-    }
-    return render(request, "blog/post_detail.html", context)
-
-
-def post_list(request):
-    if not request.user.is_authenticated():
-        return redirect("/")
-    today = timezone.now().date()
-    queryset_list = Post.objects.active().filter(category="list").order_by("-timestamp")
-
-    if request.user.is_staff or request.user.is_superuser:
-        queryset_list = Post.objects.all().filter(category="list").order_by("-timestamp")
-    query = request.GET.get("q")
-    if query:
-        queryset_list = queryset_list.filter(
-            Q(title__icontains=query)|
-            Q(content__icontains=query)|
-            Q(user__first_name__icontains=query)|
-            Q(user__last_name__icontains=query)
-
-        ).distinct()
-    paginator = Paginator(queryset_list, 10)# Show 10 posts per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        queryset = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        "object_list": queryset,
-        "title": "List",
-        "page_request_var": page_request_var,
-        "today": today,
-
-    }
-    return render(request, 'blog/post_list.html', context)
-
-
-def post_useful(request):
-    if not request.user.is_authenticated():
-        return redirect("/")
-    today = timezone.now().date()
-    queryset_list = Post.objects.active().filter(category="useful").order_by("-timestamp")
-    if request.user.is_staff or request.user.is_superuser:
-        queryset_list = Post.objects.all().filter(category="useful").order_by("-timestamp")
-    query = request.GET.get("q")
-    if query:
-        queryset_list = queryset_list.filter(
-            Q(title__icontains=query) |
-            Q(content__icontains=query) |
-            Q(user__first_name__icontains=query) |
-            Q(user__last_name__icontains=query)
-
-        ).distinct()
-    paginator = Paginator(queryset_list, 10)  # Show 10 posts per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        queryset = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        "object_list": queryset,
-        "title": "List",
-        "page_request_var": page_request_var,
-        "today": today,
-
-    }
-    return render(request, "blog/post_useful.html", context)
-
-
-def post_events(request):
-    if not request.user.is_authenticated():
-        return redirect("/")
-    today = timezone.now().date()
-    queryset_list = Post.objects.active().filter(category="events").order_by("-timestamp")
-
-    if request.user.is_staff or request.user.is_superuser:
-        queryset_list = Post.objects.all().filter(category="events").order_by("-timestamp")
-    query = request.GET.get("q")
-    if query:
-        queryset_list = queryset_list.filter(
-            Q(title__icontains=query) |
-            Q(content__icontains=query) |
-            Q(user__first_name__icontains=query) |
-            Q(user__last_name__icontains=query)
-
-        ).distinct()
-    paginator = Paginator(queryset_list, 10)  # Show 10 posts per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        queryset = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        "object_list": queryset,
-        "title": "List",
-        "page_request_var": page_request_var,
-        "today": today,
-
-    }
-    return render(request, "blog/post_events.html", context )
-
-
-def post_other(request):
-    if not request.user.is_authenticated():
-        return redirect("/")
-    today = timezone.now().date()
-    queryset_list = Post.objects.active().filter(category="other").order_by("-timestamp")
-
-    if request.user.is_staff or request.user.is_superuser:
-        queryset_list = Post.objects.all().filter(category="other").order_by("-timestamp")
-    query = request.GET.get("q")
-    if query:
-        queryset_list = queryset_list.filter(
-            Q(title__icontains=query) |
-            Q(content__icontains=query) |
-            Q(user__first_name__icontains=query) |
-            Q(user__last_name__icontains=query)
-
-        ).distinct()
-    paginator = Paginator(queryset_list, 10)  # Show 10 posts per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        queryset = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        "object_list": queryset,
-        "title": "List",
-        "page_request_var": page_request_var,
-        "today": today,
-
-    }
-    return render(request, "blog/post_other.html", context)
-
-
 def post_update(request, slug=None):
     # if not request.user.is_staff or not request.user.is_superuser:
     #     raise Http404
     if not request.user.is_authenticated():
-        return redirect("/")
+        return redirect("log_in")
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance,)
     if form.is_valid():
@@ -224,7 +53,7 @@ def post_update(request, slug=None):
 
 def post_delete(request, slug=None):
     if not request.user.is_authenticated():
-        return redirect("/")
+        return redirect("log_in")
     # if not request.user.is_staff or not request.user.is_superuser:
     #     raise Http404
     instance = get_object_or_404(Post, slug=slug)
@@ -233,6 +62,83 @@ def post_delete(request, slug=None):
     return redirect("posts:list")
 
 
-def post_about_us(request):
+def post_detail(request, slug=None):
+    if not request.user.is_authenticated():
+        return redirect("log_in")
+    instance = get_object_or_404(Post, slug=slug)
+    if instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+    share_string = quote_plus(instance.content)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "share_string": share_string,
+    }
+    return render(request, "blog/post_detail.html", context)
 
-    return render(request, "blog/about.html")
+
+class PostView:
+    @staticmethod
+    def universal_view(html, cat, request):
+        if not request.user.is_authenticated():
+            return redirect("log_in")
+        today = timezone.now().date()
+        queryset_list = Post.objects.active().filter(category=cat).order_by("-timestamp")
+
+        if request.user.is_staff or request.user.is_superuser:
+            queryset_list = Post.objects.all().filter(category=cat).order_by("-timestamp")
+        query = request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+
+            ).distinct()
+        paginator = Paginator(queryset_list, 10)  # Show 10 posts per page
+        page_request_var = 'page'
+        page = request.GET.get(page_request_var)
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            queryset = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            queryset = paginator.page(paginator.num_pages)
+
+        context = {
+            "object_list": queryset,
+            "title": "List",
+            "page_request_var": page_request_var,
+            "today": today,
+
+        }
+        return render(request, html, context)
+
+
+def post_list(request):
+    x = PostView()
+    return x.universal_view('blog/post_list.html', 'list', request)
+
+
+def post_useful(request):
+    x = PostView()
+    return x.universal_view('blog/post_useful.html', 'useful', request)
+
+
+def post_events(request):
+    x = PostView()
+    return x.universal_view('blog/post_events.html', 'events', request)
+
+
+def post_other(request):
+    x = PostView()
+    return x.universal_view('blog/post_other.html', 'other', request)
+
+
+
+
+
